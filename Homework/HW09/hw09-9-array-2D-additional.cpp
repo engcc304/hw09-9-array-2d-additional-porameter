38a563526ed6cb1265440b90bb6e96be6a3632bc
porameter <paramet_su66@live.rmutl.ac.th>
2023-09-21 08:17:58 +0700

    update hw 09-9

--- Homework/HW09/hw09-9-array-2D-additional.cpp
+++ Homework/HW09/hw09-9-array-2D-additional.cpp
@@ -21,3 +21,61 @@
             8 9 10
 
 */
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols ;
+
+    // รับข้อมูลของ Array1
+    printf( "Array1 element:\n" ) ;
+    printf( "Enter the number of rows: " ) ;
+    scanf( "%d" , &rows ) ;
+    printf( "Enter the number of columns: " ) ;
+    scanf( "%d" , &cols ) ;
+
+    int array1[rows][cols] ;
+
+    printf( "Input elements for Array1:\n" ) ;
+    for ( int i = 0; i < rows; i++ ) {
+        for ( int j = 0; j < cols; j++ ) {
+            scanf( "%d", &array1[i][j] ) ;
+        }
+    }
+
+    // รับข้อมูลของ Array2
+    printf( "Array2 element:\n" ) ;
+    printf( "Enter the number of rows: " ) ;
+    scanf( "%d", &rows ) ;
+    printf( "Enter the number of columns: " ) ;
+    scanf( "%d" , &cols ) ;
+
+    int array2[rows][cols] ;
+
+    printf( "Input elements for Array2:\n" ) ;
+    for ( int i = 0; i < rows; i++ ) {
+        for ( int j = 0; j < cols; j++ ) {
+            scanf( "%d" , &array2[i][j] ) ;
+        }
+    }
+
+    // สร้างและคำนวณผลรวมใน Array3
+    int array3[rows][cols] ;
+
+    for ( int i = 0; i < rows; i++ ) {
+        for ( int j = 0; j < cols; j++ ) {
+            array3[i][j] = array1[i][j] + array2[i][j] ;
+        }
+    }
+
+    // แสดงผลลัพธ์ของ Array3
+    printf( "Array3 (Sum of Array1 and Array2):\n" ) ;
+    for ( int i = 0; i < rows; i++ ) {
+        for ( int j = 0; j < cols; j++ ) {
+            printf( "%d " , array3[i][j] ) ;
+        }
+        printf( "\n" ) ;
+    }
+
+    return 0 ;
+}
